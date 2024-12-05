ecf9e24c7271e077f077c36b600ff1b0a4f14997
rabi320 <ryonatan6@gmail.com>
2024-12-05 08:46:59 +0000

    new dataframe

--- Users/yonatanr/Predictoos_Demo/prdcts_demo.py
+++ Users/yonatanr/Predictoos_Demo/prdcts_demo.py
@@ -41,7 +41,7 @@ def generate_text(prompt, sys_msg, examples=[]):
 
 
 
-inv_st_df = pd.read_csv('Inventory_strategy_table.csv',index = False)
+inv_st_df = pd.read_csv('Inventory_strategy_table.csv')
 
 # Display the DataFrame with text wrapping
 st.dataframe(inv_st_df, use_container_width=True)
