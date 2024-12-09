835cd8c96fbd7e761d0280dcc4af68a1f28b8081
rabi320 <ryonatan6@gmail.com>
2024-12-09 15:31:28 +0000

    string fix

--- Users/yonatanr/Predictoos_Demo/prdcts_demo.py
+++ Users/yonatanr/Predictoos_Demo/prdcts_demo.py
@@ -181,7 +181,7 @@ if selected_combinations:
     st.write('AI Inventory Strategy')
     for index, row in filtered_df.iterrows():
         tooltip_text = f"{row['IS']}"
-        st.write(f"<div class='tooltip'>{row['CUSTOMER_CODE']+' '+row['MATERIAL_NAME']}<span class='tooltiptext'>{tooltip_text}</span></div>",
+        st.write(f"<div class='tooltip'>{str(row['CUSTOMER_CODE'])+' '+row['MATERIAL_NAME']}<span class='tooltiptext'>{tooltip_text}</span></div>",
                 unsafe_allow_html=True)
         
 else:
