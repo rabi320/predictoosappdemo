b5ffc967325142fd468da2da65e9b4e81c7e3575
rabi320 <ryonatan6@gmail.com>
2024-12-09 14:43:44 +0000

    Fixing columns p3

--- Users/yonatanr/Predictoos_Demo/prdcts_demo.py
+++ Users/yonatanr/Predictoos_Demo/prdcts_demo.py
@@ -115,8 +115,8 @@ st.markdown("""
 
 # Display DataFrame with tooltips
 for index, row in df.iterrows():
-    tooltip_text = f"This is {row['MATERIAL_NAME']}, identified by code {row['MATERIAL_NAME']}"
-    st.write(f"<div class='tooltip'>{row['Material Name']}<span class='tooltiptext'>{tooltip_text}</span></div>",
+    tooltip_text = f"This is {row['MATERIAL_NAME']}, identified by code {row['CUSTOMER_CODE']}"
+    st.write(f"<div class='tooltip'>{row['MATERIAL_NAME']}<span class='tooltiptext'>{tooltip_text}</span></div>",
              unsafe_allow_html=True)
     
 
@@ -125,7 +125,7 @@ for index, row in df.iterrows():
 st.title("Filter DataFrame by Unique Combinations of Material and Customer")
 
 # Create a list of unique combinations
-unique_combinations = inv_st_df[['MATERIAL_NAME', 'MATERIAL_NAME']].drop_duplicates()
+unique_combinations = inv_st_df[['MATERIAL_NAME', 'CUSTOMER_CODE']].drop_duplicates()
 
 # Create checkboxes for each unique combination
 selected_combinations = []
